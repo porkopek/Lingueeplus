648f7432357b5f43eaad6a34e45c23ce31a472fc
José Pérez Negro <peresnegro@gmail.com>
2018-02-08 14:13:01 +0000

    popup working

--- popup/popup.js
+++ popup/popup.js
@@ -9,22 +9,24 @@ const importantUrlsPromise = browser.storage.local
 
 //render the stored urls
 const renderUrls = urls => {
-  if (urls === undefined || urls.length === 0) return;
-
-  //HTML of rendered urls
-  const urlsHTML = urls
-    .map(
-      (url, index) => `<div 
+  console.log('urls to reder ', urls);
+  const urlsHTML =
+    urls === undefined || urls.length === 0
+      ? ''
+      : urls
+          .map(
+            (url, index) => `
+        <div 
           id ="url-${index}" 
           class="important-url" 
           data-url=${url}>
             ${url}
             <button id="btn-${index}"class="delete-url">
               x
-            </button>
+            </button>            
         </div>`
-    )
-    .reduce((a, b) => a + b);
+          )
+          .reduce((a, b) => a + b);
 
   // append HTML
   var urlsSection = document.querySelector('#urlsSection');
@@ -40,8 +42,47 @@ const renderUrls = urls => {
 //remove url function
 const removeUrl = event => {
   const urlId = 'url-' + event.target.id.split('-')[1];
-  console.log(document.querySelector(`#${urlId}`).dataset.url);
+  const urlToRemove = document.querySelector(`#${urlId}`).dataset.url;
+
+  browser.storage.local
+    .get('importantUrls')
+    .then(result => result['importantUrls'])
+    .then(urls => urls.filter(url => url !== urlToRemove))
+    .then(urls => {
+      console.log('novo array urls após delete ', urls);
+      browser.storage.local.set({
+        importantUrls: urls
+      });
+      return urls;
+    })
+    .then(renderUrls);
 };
+
+// add url function
+const addUrl = () => {
+  const url = document.querySelector('#urlInput').value;
+  console.log('url to add ', url);
+  browser.storage.local
+    .get('importantUrls')
+    .then(result => result['importantUrls'])
+    .then(urls => {
+      console.log('array sem url added', urls);
+      var newUrls = urls === undefined ? [url] : [...urls, url];
+      console.log('array com url added ', newUrls);
+      return newUrls;
+    })
+    .then(urls2 => {
+      browser.storage.local.set({
+        importantUrls: urls2
+      });
+      return urls2;
+    })
+    .then(renderUrls);
+};
+
+//add url button
+var addUrlButton = document.querySelector('#addUrlButton');
+addUrlButton.addEventListener('click', addUrl);
 
 //execute the promise
 importantUrlsPromise.then(renderUrls);
